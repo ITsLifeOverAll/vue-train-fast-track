4647323c90acf96a530d64e7b282be85d468523a
Art Tsai <arttsai@gmail.com>
2025-04-14 16:56:21 +0800

    定義 props

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref, computed } from 'vue';
 import Counter from './components/Counter.vue';
+import VoteItem from './components/VoteItem.vue';
 </script>
 
 <template>
-  <div class="container m-4">
+  <div class="container p-4">
     <h1 class="title is-3 has-text-centered">Hello Vue!</h1>
-    <Counter />
+    <VoteItem />
   </div>
 </template>
