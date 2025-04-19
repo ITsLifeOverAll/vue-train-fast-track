557cf1a56d128a187b5ded8553455cabc8af3d84
Art Tsai <arttsai@gmail.com>
2025-04-19 15:00:50 +0800

    讓 component 中的按讚，具有響應能力

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container p-4">
     <h1 class="title is-3 has-text-centered">Hello Vue!</h1>
-    <div v-for="item in voteList" class="p-4">
-      <VoteItem v-bind="item" />
+    <div v-for="item in voteItems" class="p-4">
+      <VoteItem :item="item" />
     </div>
   </div>
 </template>
@@ -15,6 +15,8 @@ import voteList from './data/votes.js'
 voteList.sort((a, b) => {
   return b.votes - a.votes;
 });
+
+const voteItems = ref(voteList);
 
 
 </script>
--- src/components/VoteItem.vue
+++ src/components/VoteItem.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="card columns ">
         <div class="is-one-third image is-64x64 m-4">
-            <img v-bind:src="imageUrl" alt="Placeholder image">
+            <img v-bind:src="item.imageUrl" alt="Placeholder image">
         </div>
 
         <div class="card-content column">
             <div class="media">
                 <div class="media-content m-4">
-                    <span class="title is-4">{{ name }}</span>
+                    <span class="title is-4">{{ item.name }}</span>
                     <p>
-                        {{ description }}
+                        {{ item.description }}
                     </p>
                 </div>
                 <div class="media-right">
-                    ^ <span class="tag is-info mr-4">{{ votes }}</span>
+                    ^ <span class="tag is-info mr-4">{{ item.votes }}</span>
                     <button class="button is-small" @click="voteUp">
                         <span class="icon is-small">
                             <i class="fas fa-thumbs-up"></i>
@@ -27,25 +27,13 @@
 
 <script setup>
 const props = defineProps({
-    name: {
-        type: String,
+    item: {
+        type: Object,
         required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    imageUrl: {
-        type: String,
-        required: true
-    },
-    votes: {
-        type: Number,
-        default: 0
     }
 })
 
 const voteUp = () => {
-    props.votes++
+    props.item.votes++
 }
 </script>
