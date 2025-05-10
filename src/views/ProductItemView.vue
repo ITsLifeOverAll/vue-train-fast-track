8be6887a4dde9fe754c2d37f9b0e98864e002a1e
Art Tsai <arttsai@gmail.com>
2025-05-10 18:01:49 +0800

    使用 useRoute()

--- src/views/ProductItemView.vue
+++ src/views/ProductItemView.vue
@@ -31,6 +31,7 @@
 <script setup>
 import products from '@/data/foods'
 import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
 const props = defineProps({
     id: {
@@ -39,13 +40,12 @@ const props = defineProps({
     }
 })
 
+const route = useRoute()
 
 const product = computed(() => {
-    const product = products.find(product => product.id == props.id)
-    if (!product) {
-        throw new Error('Product not found')
-    }
-    return product
+    const id = route.params.id
+    return products.find(product => product.id === Number(id))
 })
+
 
 </script>
