def28a5beb650124c79ee36166e90864502728ad
Art Tsai <arttsai@gmail.com>
2025-05-23 15:15:20 +0800

    建立 User Store

---
+++ src/stores/user.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+
+export const useUserStore = defineStore('user', () => {
+  const userId = ref(0)
+  const userName = computed(() => {
+    ;`user ${userId.value}`
+  })
+  const isLogin = computed(() => {
+    return userId.value > 0
+  })
+  const login = (id) => {
+    userId.value = id
+  }
+  const logout = () => {
+    userId.value = 0
+  }
+
+  return { userId, userName, isLogin, login, logout }
+})
