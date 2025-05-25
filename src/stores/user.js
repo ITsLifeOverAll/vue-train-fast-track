ef041186361aca94986eceaa42e372a2b5a91fbc
Art Tsai <arttsai@gmail.com>
2025-05-25 20:56:46 +0800

    留存狀態在 Cookie 的做法

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,7 +2,9 @@ import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 
 export const useUserStore = defineStore('user', () => {
-  const userId = ref(0)
+  const Cookies = window.Cookies
+  const userId = ref(Cookies.get('userId') ? parseInt(Cookies.get('userId')) : 0)
+
   const userName = computed(() => {
     return `user ${userId.value}`
   })
@@ -11,9 +13,11 @@ export const useUserStore = defineStore('user', () => {
   })
   const login = (id) => {
     userId.value = id
+    Cookies.set('userId', id, { expires: 7 }) // 設定 cookie 有效期為 7 天
   }
   const logout = () => {
     userId.value = 0
+    Cookies.remove('userId') // 移除 cookie
   }
 
   return { userId, userName, isLogin, login, logout }
