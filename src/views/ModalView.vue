48e8abe185ad5c830c2cc769c66c008e703a6fe2
Art Tsai <arttsai@gmail.com>
2025-06-13 16:51:39 +0800

    對話窗輸入檢查

--- src/views/ModalView.vue
+++ src/views/ModalView.vue
@@ -5,7 +5,7 @@
             <button class="button is-primary" @click="showModal = true">Launch Card Modal</button>
             <CardModal v-model="showModal">
                 <template #header>
-                    請提供資訊
+                    請提供您的資訊
                 </template>
                 <template #footer>
                     <div class="buttons">
@@ -19,6 +19,7 @@
                     <div class="control">
                         <input id="name" v-model="inquery.name" class="input" type="text" placeholder="Text input">
                     </div>
+                    <span v-if="error.name" class="help is-danger">{{ error.name }}</span>
                 </div>
 
                 <div class="field">
@@ -26,6 +27,7 @@
                     <div class="control">
                         <input id="age" v-model="inquery.age" class="input" type="text" placeholder="Text input">
                     </div>
+                    <span v-if="error.age" class="help is-danger">{{ error.age }}</span>
                 </div>
             </CardModal>
         </div>
@@ -41,6 +43,45 @@ const inquery = ref({
     name: '',
     age: ''
 });
+
+const error = ref({
+    name: '',
+    age: ''
+});
+
+const validate = () => {
+    error.value.name = '';
+    error.value.age = '';
+
+    if (!inquery.value.name) {
+        error.value.name = '姓名不能為空';
+    }
+
+    const age = Number(inquery.value.age);
+    if (!inquery.value.age) {
+        error.value.age = '年紀不能為空';
+    } else if (isNaN(age) || age <= 15) {
+        error.value.age = '年紀必須大於 15 歲';
+    }
+
+    return !error.value.name && !error.value.age;
+};
+
+const save = () => {
+    if (!validate()) {
+        return;
+    }
+    showModal.value = false;
+};
+
+const cancel = () => {
+    inquery.value.name = '';
+    inquery.value.age = '';
+    error.value.name = '';
+    error.value.age = '';
+
+    showModal.value = false;
+};
 
 
 </script>
